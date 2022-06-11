52fac9b776584afaf22c67f8de4d5e3c2063ff8d
jquizo <45918580+jquizo@users.noreply.github.com>
2022-06-12 02:14:50 +1000

    initial commit

---
+++ app.js
@@ -0,0 +1,44 @@
+// NAVBAR TOGGLE BUTTON
+
+const toggleButton = document.getElementsByClassName('toggle-button')[0]
+const navbarLinks = document.getElementsByClassName('navbar-links')[0]
+
+toggleButton.addEventListener('click', () => {
+  navbarLinks.classList.toggle('active')
+})
+
+// DARK MODE TOGGLE 
+
+/* Body */
+const body = document.querySelector('body');
+
+// Dark Mode Action
+let darkMode = localStorage.getItem("darkMode");
+const darkModeToggle = document.querySelector('.dark-mode-button');
+const darkModeToggleFooter = document.querySelector('footer .dark-mode-button');
+
+// Enable Dark Mode
+const enableDarkMode = () => {
+    body.classList.add("dark-mode");
+    localStorage.setItem("darkMode", "enabled")
+}
+
+// Disable Dark Mode
+const disableDarkMode = () => {
+    body.classList.remove("dark-mode");
+    localStorage.setItem("darkMode", null)
+}
+
+if (darkMode == "enabled") {
+    enableDarkMode();
+}
+
+// Desktop Button
+darkModeToggle.addEventListener('click', () => {
+    darkMode = localStorage.getItem("darkMode");
+    if (darkMode !== "enabled") {
+        enableDarkMode();
+    } else {
+        disableDarkMode();
+    }
+})
